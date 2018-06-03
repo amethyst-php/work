7713d4cde9578c05acd960c2f60708d58e7ddf6a
railken <railken@protonmail.ch>
2018-06-03 16:26:54 +0200

    update filename migration

--- src/WorkServiceProvider.php
+++ src/WorkServiceProvider.php
@@ -19,7 +19,7 @@ class WorkServiceProvider extends ServiceProvider
 
         if (!class_exists('CreateWorksTable')) {
             $this->publishes([
-                __DIR__.'/../database/migrations/create_works_table.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_works_table.php'),
+                __DIR__.'/../database/migrations/create_works_table.php.stub' => database_path('migrations/'.(new \DateTime())->format("Y_m_d_His.u").'_create_works_table.php'),
             ], 'migrations');
         }
     }
